7c1f72f77c372af220a4d39c899db9e3542dcf32
arshad-ashraf-a <arshad.ashraf.a@outlook.com>
2023-11-01 17:16:02 +0400

    initial commit

---
+++ Model/TEMP_TESTING_TABLE.cs
@@ -0,0 +1,9 @@
+﻿namespace net_core_crud.Model
+{
+    public class TEMP_TESTING_TABLE
+    {
+        public string FIELD1 { get; set; } 
+        public string FIELD2 { get; set; } 
+        public DateTime? INSERTDATE { get; set; } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using net_core_crud.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<SchoolContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("sqlconnect")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
